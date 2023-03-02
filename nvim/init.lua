26f5bf585d7a94118caf9da116f5f701174d2da2
Vinson Chuong <vinsonchuong@gmail.com>
2023-03-01 19:47:27 -0800

    Starting to move per-package config into Packer config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,108 +1,313 @@
 -- luacheck: globals vim
 
-require("packer").startup(function(use)
-	use("williamboman/mason.nvim")
-	use("WhoIsSethDaniel/mason-tool-installer.nvim")
-	use("jayp0521/mason-null-ls.nvim")
+---@param name string
+-- local function package(spec)
+-- 	local packer_spec = {}
+-- 	packer_spec.name = spec[1]
+--
+-- 	local org, repo = string.match(packer_spec.name, '^([^/]*)/([^/]*)$')
+--
+-- 	if spec.config == true then
+-- 		local file = string.match(repo, '^([^.]*)%.nvim$')
+-- 		packer_spec.config = function()
+-- 			require(file).setup()
+-- 		end
+-- 	elseif typeof(spec.config) == 'table' then
+-- 		local file = string.match(repo, '^([^.]*)%.nvim$')
+-- 		packer_spec.config = function()
+-- 			require(file).setup(spec.config)
+-- 		end
+-- 	end
+--
+-- 	return packer_spec
+-- end
 
-	use("neovim/nvim-lspconfig")
-	use("folke/neodev.nvim")
+require("packer").startup({
+	{
+		{
+			"nvim-treesitter/nvim-treesitter",
+			config = function()
+				require("nvim-treesitter.configs").setup({
+					ensure_installed = { "lua", "vim", "bash", "html", "css", "javascript" },
+					sync_install = false,
+					auto_install = true,
+					highlight = {
+						enable = true,
+					},
+					indent = {
+						enable = true,
+					},
+				})
 
-	use("nvim-lua/plenary.nvim")
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-	use("nvim-telescope/telescope.nvim")
-	use("jose-elias-alvarez/null-ls.nvim")
+				vim.opt.foldmethod = "expr"
+				vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/nvim-cmp")
-	use("saadparwaiz1/cmp_luasnip")
-	use("L3MON4D3/LuaSnip")
+				require("nvim-treesitter.install").update()
+			end,
+		},
+		{
+			"nvim-telescope/telescope.nvim",
+			requires = {
+				"nvim-lua/plenary.nvim",
+				{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+			},
+			config = function()
+				require("telescope").setup({
+					pickers = {
+						find_files = {
+							find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+						},
+					},
+				})
+			end,
+		},
+		{
+			"numToStr/Comment.nvim",
+			config = function()
+				require("Comment").setup()
+			end,
+		},
+		{
+			"Tsuzat/NeoSolarized.nvim",
+			config = function()
+				require("NeoSolarized").setup({
+					style = "light",
+					on_highlights = function(highlights, colors)
+						highlights.Normal.fg = colors.fg2
+						highlights.NormalNC.fg = colors.fg2
+						highlights.NormalSB.fg = colors.fg2
+						highlights.WinSeparator.fg = colors.bg1
+					end,
+				})
+				vim.cmd("colorscheme NeoSolarized")
+				vim.opt.background = "light"
+			end,
+		},
+		{
+			"nvim-lualine/lualine.nvim",
+			after = { "NeoSolarized.nvim" },
+			config = function()
+				require("lualine").setup({
+					options = {
+						theme = "NeoSolarized",
+						icons_enabled = false,
+						component_separators = { left = "", right = "" },
+						section_separators = { left = "", right = "" },
+					},
+				})
+			end,
+		},
+		{ "vinsonchuong/vim-stdtabs" },
+		{ "sheerun/vim-polyglot" },
+		{
+			"williamboman/mason.nvim",
+			config = function()
+				require("mason").setup()
+			end,
+		},
+		{
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			after = { "mason.nvim" },
+			config = function()
+				require("mason-tool-installer").setup({
+					ensure_installed = {
+						"lua-language-server",
+						"bash-language-server",
+						"typescript-language-server",
+						"marksman",
+						"json-lsp",
+						"luacheck",
+						"stylua",
+					},
+					auto_update = true,
+				})
+			end,
+		},
+		{
+			"williamboman/mason-lspconfig.nvim",
+			requires = { "neovim/nvim-lspconfig" },
+			after = { "mason.nvim" },
+			event = "User MasonToolsUpdateCompleted",
+			config = function()
+				local plugin = require("mason-lspconfig")
+				plugin.setup()
+				plugin.setup_handlers({
+					function(name)
+						require("lspconfig")[name].setup({})
+					end,
+					["lua_ls"] = function()
+						require("lspconfig").lua_ls.setup({
+							settings = {
+								Lua = {
+									workspace = {
+										checkThirdParty = false,
+									},
+								},
+							},
+						})
+					end,
+					["tsserver"] = function()
+						require("lspconfig").tsserver.setup({
+							on_attach = function(client)
+								client.server_capabilities.documentFormattingProvider = false
+								client.server_capabilities.documentRangeFormattingProvider = false
+							end,
+						})
+					end,
+				})
+			end,
+		},
+		{
+			"jayp0521/mason-null-ls.nvim",
+			requires = { "jose-elias-alvarez/null-ls.nvim" },
+			after = { "mason.nvim", "null-ls.nvim" },
+			event = "User MasonToolsUpdateCompleted",
+			config = function()
+				local plugin = require("mason-null-ls")
+				plugin.setup({
+					automatic_setup = true,
+				})
+				plugin.setup_handlers({})
+			end,
+		},
+		{
+			"jose-elias-alvarez/null-ls.nvim",
+			requires = { "nvim-lua/plenary.nvim" },
+			config = function()
+				local plugin = require("null-ls")
+				local augroup = vim.api.nvim_create_augroup("NullLsFormatting", {})
+				plugin.setup({
+					on_attach = function(client, bufnr)
+						if client.supports_method("textDocument/formatting") then
+							vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+							vim.api.nvim_create_autocmd("BufWritePre", {
+								group = augroup,
+								buffer = bufnr,
+								callback = function()
+									vim.lsp.buf.format({ bufnr = bufnr })
+								end,
+							})
+						end
+					end,
+					sources = {
+						plugin.builtins.diagnostics.xo.with({
+							command = "xo_d",
+						}),
+						plugin.builtins.formatting.eslint_d.with({
+							command = "xo_d",
+							args = { "--fix", "--stdin", "--stdin-filename", "$FILENAME" },
+						}),
+					},
+				})
+			end,
+		},
+		{
+			"folke/neodev.nvim",
+			requires = { "neovim/nvim-lspconfig" },
+			after = { "mason-lspconfig.nvim" },
+			config = function()
+				require("neodev").setup()
+			end,
+		},
+		{
+			"hrsh7th/cmp-nvim-lsp",
+			requires = { "hrsh7th/nvim-cmp" },
+		},
+		{
+			"saadparwaiz1/cmp_luasnip",
+			requires = { "hrsh7th/nvim-cmp" },
+		},
+		{ "L3MON4D3/LuaSnip" },
+		{
+			"hrsh7th/nvim-cmp",
+			after = { "nvim-lspconfig", "LuaSnip" },
+			config = function()
+				local plugin = require("cmp")
+				plugin.setup({
+					snippet = {
+						expand = function(args)
+							require("luasnip").lsp_expand(args.body)
+						end,
+					},
+					mapping = {
+						["<C-p>"] = plugin.mapping.select_prev_item(),
+						["<C-n>"] = plugin.mapping.select_next_item(),
+						["<C-e>"] = plugin.mapping.abort(),
+						["<C-y>"] = plugin.mapping.confirm({
+							behavior = plugin.ConfirmBehavior.Replace,
+							select = true,
+						}),
+					},
+					sources = {
+						{ name = "nvim_lsp" },
+						{ name = "luasnip" },
+					},
+					experimental = {
+						native_menu = false,
+						ghost_text = true,
+					},
+				})
+			end,
+		},
+		{
+			"folke/trouble.nvim",
+			config = function()
+				require("trouble").setup()
+			end,
+		},
 
-	use("nvim-tree/nvim-web-devicons")
+		{ "tpope/vim-fugitive" },
+		{ "gregsexton/gitv" },
 
-	use("kkharji/lspsaga.nvim")
+		{ "sindrets/diffview.nvim" },
 
-	use("folke/trouble.nvim")
+		{
+			"zbirenbaum/copilot.lua",
+			config = function()
+				require("copilot").setup({
+					filetypes = {
+						text = false,
+					},
+					copilot_node_command = vim.fn.expand("$HOME") .. "/opt/n/n/versions/node/16.18.1/bin/node",
+				})
+			end,
+		},
 
-	use("tpope/vim-fugitive")
-	use("gregsexton/gitv")
+		{ "mbbill/undotree" },
+		{ "tpope/vim-vinegar" },
 
-	use("sindrets/diffview.nvim")
+		{
+			"vim-test/vim-test",
+			config = function()
+				vim.g["test#javascript#ava#file_pattern"] = "test\\.js$"
+			end,
+		},
 
-	use("zbirenbaum/copilot.lua")
+		{ "thinca/vim-textobj-between" },
+		{ "glts/vim-textobj-comment" },
+		{ "kana/vim-textobj-entire", requires = "kana/vim-textobj-user" },
+		{ "kana/vim-textobj-fold", requires = "kana/vim-textobj-user" },
+		{ "kana/vim-textobj-indent", requires = "kana/vim-textobj-user" },
+		{ "kana/vim-textobj-lastpat", requires = "kana/vim-textobj-user" },
+		{ "kana/vim-textobj-line", requires = "kana/vim-textobj-user" },
+		{ "saaguero/vim-textobj-pastedtext" },
+		{ "whatyouhide/vim-textobj-xmlattr" },
 
-	use("numToStr/Comment.nvim")
-
-	use("Tsuzat/NeoSolarized.nvim")
-	use("itchyny/lightline.vim")
-
-	use("mbbill/undotree")
-	use("tpope/vim-vinegar")
-	use("vinsonchuong/vim-stdtabs")
-	use("sheerun/vim-polyglot")
-
-	use("vim-test/vim-test")
-	use("tpope/vim-dispatch")
-
-	use("kana/vim-textobj-user")
-	use("thinca/vim-textobj-between")
-	use("glts/vim-textobj-comment")
-	use({ "kana/vim-textobj-entire", after = "kana/vim-textobj-user" })
-	use({ "kana/vim-textobj-fold", after = "kana/vim-textobj-user" })
-	use({ "kana/vim-textobj-indent", after = "kana/vim-textobj-user" })
-	use({ "kana/vim-textobj-lastpat", after = "kana/vim-textobj-user" })
-	use({ "kana/vim-textobj-line", after = "kana/vim-textobj-user" })
-	use("sgur/vim-textobj-parameter")
-	use("saaguero/vim-textobj-pastedtext")
-	use({ "kana/vim-textobj-syntax", after = "kana/vim-textobj-user" })
-	use("whatyouhide/vim-textobj-xmlattr")
-
-	use("AndrewRadev/splitjoin.vim")
-	use("bronson/vim-visual-star-search")
-	use("jiangmiao/auto-pairs")
-	use("mhinz/vim-grepper")
-	use("tpope/vim-repeat")
-	use("tpope/vim-abolish")
-	use("tpope/vim-characterize")
-	use("tpope/vim-endwise")
-	use("tpope/vim-eunuch")
-	use("tpope/vim-surround")
-	use("tpope/vim-speeddating")
-	use("tpope/vim-unimpaired")
-	use("lambdalisue/suda.vim")
-end)
-
-require("mason").setup()
-require("mason-tool-installer").setup({
-	ensure_installed = {
-		"lua-language-server",
-		"bash-language-server",
-		"typescript-language-server",
-		"json-lsp",
+		{ "AndrewRadev/splitjoin.vim" },
+		{ "bronson/vim-visual-star-search" },
+		{ "jiangmiao/auto-pairs" },
+		{ "mhinz/vim-grepper" },
+		{ "tpope/vim-repeat" },
+		{ "tpope/vim-abolish" },
+		{ "tpope/vim-characterize" },
+		{ "tpope/vim-endwise" },
+		{ "tpope/vim-eunuch" },
+		{ "tpope/vim-surround" },
+		{ "tpope/vim-speeddating" },
+		{ "tpope/vim-unimpaired" },
+		{ "lambdalisue/suda.vim" },
 	},
-	auto_update = true,
 })
-
-require("copilot").setup({
-	filetypes = {
-		text = false,
-	},
-	copilot_node_command = vim.fn.expand("$HOME") .. "/opt/n/n/versions/node/16.18.1/bin/node",
-})
-
-local colors = require("NeoSolarized.colors").setup()
-require("NeoSolarized").setup()
-vim.api.nvim_command("colorscheme NeoSolarized")
-vim.opt.background = "light"
-
-vim.api.nvim_command("highlight Normal guifg=" .. colors.fg2)
-vim.api.nvim_command("highlight NormalNC guifg=" .. colors.fg2)
-vim.api.nvim_command("highlight NormalSB guifg=" .. colors.fg2)
-vim.api.nvim_command("highlight WinSeparator guifg=" .. colors.bg1)
-
-vim.g.lightline = { colorscheme = "solarized" }
 
 vim.g.mapleader = " "
 
@@ -112,162 +317,34 @@ vim.opt.numberwidth = 5
 vim.opt.undofile = true
 vim.opt.backupcopy = "yes"
 vim.opt.diffopt:append({ "vertical" })
-vim.opt.foldmethod = "syntax"
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-local nvim_lsp = require("lspconfig")
-local cmp = require("cmp")
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-local luasnip = require("luasnip")
-
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "lua", "vim", "bash", "html", "css", "javascript" },
-	sync_install = false,
-	highlight = {
-		enable = true,
-	},
-})
-
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = {
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<C-y>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-	},
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-	},
-	experimental = {
-		native_menu = false,
-		ghost_text = true,
-	},
-})
-
-require("lspsaga").init_lsp_saga({
-	rename_action_keys = {
-		quit = "<Esc>",
-		exec = "<CR>",
-	},
-	use_saga_diagnostic_sign = false,
-	code_action_prompt = {
-		enable = false,
-		sign = false,
-	},
-})
-
-require("trouble").setup()
-
-require("Comment").setup({
-	mappings = {
-		basic = true,
-		extra = true,
-	},
-})
-
-require("telescope").setup({
-	pickers = {
-		find_files = {
-			find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
-		},
-	},
-})
-
-require("neodev").setup()
-
-local on_attach = function(client, bufnr) -- luacheck: ignore
-	-- local function buf_set_keymap(...)
-	-- 	vim.api.nvim_buf_set_keymap(bufnr, ...)
-	-- end
-
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
-
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-	-- local opts = { noremap = true, silent = true }
-	--- buf_set_keymap('n', '<Leader>ra', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-end
-
-nvim_lsp.sumneko_lua.setup({
-	on_attach = on_attach,
-	flags = {
-		debounce_text_changes = 150,
-	},
-	capabilities = cmp_nvim_lsp.default_capabilities(),
-	settings = {
-		Lua = {
-			workspace = {
-				checkThirdParty = false,
-			},
-		},
-	},
-})
-
-nvim_lsp.tsserver.setup({
-	on_attach = on_attach,
-	flags = {
-		debounce_text_changes = 150,
-	},
-	capabilities = cmp_nvim_lsp.default_capabilities(),
-})
-
-local null_ls = require("null-ls")
-local augroup = vim.api.nvim_create_augroup("NullLsFormatting", {})
-null_ls.setup({
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
-	sources = {
-		null_ls.builtins.diagnostics.luacheck,
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.xo.with({
-			command = "xo_d",
-		}),
-		null_ls.builtins.formatting.eslint_d.with({
-			command = "xo_d",
-			args = { "--fix", "--stdin", "--stdin-filename", "$FILENAME" },
-		}),
-	},
-})
-
-require("mason-null-ls").setup({
-	ensure_installed = {
-		"luacheck",
-		"stylua",
-	},
-})
 
 vim.g.suda_smart_edit = true
 
-vim.keymap.set("n", "K", ":Lspsaga hover_doc<CR>", { silent = true })
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers["textDocument/hover"], {
+	border = "single",
+	width = 60,
+})
+vim.lsp.handlers["textDocument/codeAction"] = vim.lsp.with(vim.lsp.handlers["textDocument/codeAction"], {
+	border = "single",
+	width = 60,
+})
+
+vim.diagnostic.config({
+	float = {
+		border = "single",
+	},
+})
+
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
 vim.keymap.set("n", "gd", "<Cmd>Telescope lsp_definitions<CR>")
 vim.keymap.set("n", "gr", "<Cmd>Telescope lsp_references<CR>")
 vim.keymap.set("n", "gs", "<Cmd>Telescope lsp_document_symbols<CR>")
 vim.keymap.set("n", "gS", "<Cmd>Telescope lsp_workspace_symbols<CR>")
-vim.keymap.set("n", "<Leader>r", ":Lspsaga code_action<CR>")
-vim.keymap.set("n", "<Leader>e", ":Lspsaga show_line_diagnostics<CR>", { silent = true })
+
+vim.keymap.set("n", "<Leader>r", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<Leader>e", vim.diagnostic.open_float)
+
 vim.keymap.set("n", "<Leader>E", "<Cmd>TroubleToggle workspace_diagnostics<CR>")
 vim.keymap.set("n", "<Leader>f", "<Cmd>Telescope find_files<CR>")
 vim.keymap.set("n", "<Leader>S", "<Cmd>Telescope live_grep<CR>")
